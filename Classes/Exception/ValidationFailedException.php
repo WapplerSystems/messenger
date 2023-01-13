5c5986e56dfcf2dbdf55cbed8242fcccf32edaec
Sven Wappler <info@wappler.systems>
2023-01-13 15:44:28 +0100

    coding standards

--- Classes/Exception/ValidationFailedException.php
+++ Classes/Exception/ValidationFailedException.php
@@ -1,6 +1,12 @@
 <?php
 declare(strict_types=1);
 
+/*
+ * This file is part of the "messenger" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
 
 namespace WapplerSystems\Messenger\Exception;
 
